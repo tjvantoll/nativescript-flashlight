d4d7738c0cc9bf56dee92a4c6921f4fdbf103d40
Brad Martin <bradwaynemartin@gmail.com>
2016-08-18 23:12:46 -0500

    defs

---
+++ index.d.ts
@@ -0,0 +1,15 @@
+/**
+ * Check if the device has flashlight capability.
+ */
+export function isAvailable();
+
+
+/**
+* Turn the flashlight ON.
+*/
+export function on();
+
+/**
+* Turn the flashlight OFF.
+*/
+export function off();
